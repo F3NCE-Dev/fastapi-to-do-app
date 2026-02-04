d9b3e56c2429eb293a129c172e3b0c0c8e0c1a09
F3NCENMAN <thefrancandman@gmail.com>
2026-02-04 14:15:04 +0200

    update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,5 @@
 import uvicorn
+from config.config import settings
 
 if __name__ == "__main__":
-    uvicorn.run("main:app")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=settings.DEBUG_MODE, log_level="info")
